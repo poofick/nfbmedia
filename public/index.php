15b2487dff96f20d28623cf4a397640ed182f7a6
Taras <taras.zakrevskiy@gmail.com>
2013-10-31 13:30:43 +0300

    debug

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 //var_dump(dirname(__FILE__));
 
-error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_STRICT );
+
 ini_set('display_errors', '1');
 date_default_timezone_set('Europe/London');
 
