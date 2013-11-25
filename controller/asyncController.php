827904f60a9abd1d023eb906a8e712e0eed0f6f5
Taras <taras.zakrevskiy@gmail.com>
2013-11-25 17:41:44 +0300

    S3 updates

--- controller/asyncController.php
+++ controller/asyncController.php
@@ -25,11 +25,13 @@ class asyncController extends Controller {
 		$amazonModel = new amazonModel();
 		$conferenceModel = new conferenceModel();
 		
+		file_put_contents(DOCROOT.'logs/video_copyToAmazon.log', print_r($params, true));
+		
 		if(($params = $this->request->get_all())) {
 			$conferenceId = isset($params['conferenceId']) && $params['conferenceId'] > 0 ? (int)$params['conferenceId'] : 0;
 			$inputFile = isset($params['inputFile']) && @is_file($params['inputFile']) ? $params['inputFile'] : false;
 			
-			if($conferenceId && $inputFile && $amazonModel->copy('video', $inputFile, isset($params['deleteFlag']) && $params['deleteFlag'])) {
+			if($conferenceId && $inputFile && $amazonModel->putFileToS3('video', $inputFile, isset($params['deleteFlag']) && $params['deleteFlag'])) {
 				$conferenceModel->edit($conferenceId, array('video_url' => $amazonModel->getFileUrlFromS3('video', basename($inputFile), true)));
 			}
 		}
